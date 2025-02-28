aef4fb31d39f04fc6d77d33456b0dac20a1281f7
Juan Sebastian Gaviria Prada <jgaviria@buk.co>
2025-02-28 17:53:12 -0500

    🚀 Sytudy Symfony6

---
+++ code-symfony6-fundamentals/start/src/Controller/VinylController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use Knp\Bundle\TimeBundle\DateTimeFormatter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+use GuzzleHttp\Client;
+
+class VinylController extends AbstractController
+{
+    #[Route('/', name: 'app_homepage')]
+    public function homepage(): Response
+    {
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
+    }
+
+    #[Route('/browse/{slug}', name: 'app_browse')]
+    public function browse(DateTimeFormatter $formatter, ?string $slug = null): Response
+    {
+
+        $urlDatabase = 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json';
+
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+        $client = new Client();
+        $response = $client->request('GET', $urlDatabase);
+        $mixes = json_decode($response->getBody()->getContents(), true);
+
+        // foreach($mixes as $key => $mix)
+        // {
+        //     $mixes[$key]['ago'] =  $formatter->formatDiff($mix['createdAt']);
+        // }
+
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre,
+            'mixes' => $mixes,
+        ]);
+    }
+
+    private function getMixes(): array
+    {
+        // temporary fake "mixes" data
+        return [
+            [
+                'title' => 'PB & Jams',
+                'trackCount' => 14,
+                'genre' => 'Rock',
+                'createdAt' => new \DateTime('2021-10-02'),
+            ],
+            [
+                'title' => 'Put a Hex on your Ex',
+                'trackCount' => 8,
+                'genre' => 'Heavy Metal',
+                'createdAt' => new \DateTime('2022-04-28'),
+            ],
+            [
+                'title' => 'Spice Grills - Summer Tunes',
+                'trackCount' => 10,
+                'genre' => 'Pop',
+                'createdAt' => new \DateTime('2019-06-20'),
+            ],
+        ];
+    }
+}
